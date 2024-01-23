73fd269854f8b7f63da82e152080a9e0b00f9996
Lewis Sneddon <lewis@lewiscodes.io>
2024-01-23 18:43:24 +0000

    starting out with toolbar

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,14 +1,53 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-root',
+  selector: 'kt-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'webapp';
+  title = 'Krato';
+  inProduction?: boolean;
+  isNavbarCollapsed = true;
+  openAPIEnabled?: boolean;
+  version = '';
+  account: any | null = null;
+  entitiesNavbarItems: any[] = [];
+
+  constructor(
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  collapseNavbar(): void {
+    this.isNavbarCollapsed = true;
+  }
+
+  login(): void {
+    // this.router.navigate(['/login']);
+  }
+
+  logout(): void {
+    // this.collapseNavbar();
+    // this.loginService.logout();
+    // this.router.navigate(['']);
+  }
+
+  toggleNavbar(): void {
+    // this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
 }
